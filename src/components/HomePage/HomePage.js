ee263df7bb37daa26ef3eb42c85803d0a43b2469
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-11-30 18:05:37 +0530

    Added Home Page

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,26 @@
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import LoginIcon from '@mui/icons-material/Login';
+import './HomePage.css';
+
+function HomePage(){
+    return (
+        <div className="outer-shell">
+            <div className="inner-shell inner-1">
+                <span className="p1">Mental </span> <span className="p2">Health </span> <span className="p3">Care </span>
+                <br />
+                <Typography className="typ" variant="h6" component="div" gutterBottom>Patient Management System.</Typography>
+                <br /> <br />
+                <Button variant="outlined" color="warning" startIcon={<PersonAddIcon />}>Sign Up</Button>
+                <span className="separator"></span>
+                <Button variant="contained" color="success" endIcon={<LoginIcon />}>Sign In</Button>
+            </div>
+            <div className="inner-shell inner-2">
+                <img alt="mental-health.png" src="/images/mental-health.png" draggable="false" />
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
