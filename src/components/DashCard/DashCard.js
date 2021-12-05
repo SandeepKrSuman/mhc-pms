db0b9ba65dad72d7909171364c8e7aa4592bfdf9
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-05 13:37:49 +0530

    Added Css to DashCard

--- src/components/DashCard/DashCard.js
+++ src/components/DashCard/DashCard.js
@@ -1,13 +1,14 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import './DashCard.css';
 
 export default function DashCard(props) {
 
   const handleClick = () => { console.log("clicked! "); };
 
   return (
-    <Card sx={{ maxWidth: "100%", textAlign: "center", cursor: "pointer" }} variant="outlined" onClick={handleClick}>
+    <Card className="card-bg" sx={{ maxWidth: "100%"}} variant="outlined" onClick={handleClick}>
       <CardContent>
         <br /> <br />
         <Typography variant="h4" component="div">
