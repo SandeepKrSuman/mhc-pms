b360485f7a4f31064465f4afdb2b869fc238dfc1
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-06 16:09:34 +0530

    added navigation for card click

--- src/components/DashCard/DashCard.js
+++ src/components/DashCard/DashCard.js
@@ -1,14 +1,21 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 import './DashCard.css';
 
 export default function DashCard(props) {
 
-  const handleClick = () => { console.log("clicked! "); };
+  let navigate = useNavigate();
+
+  function handleClick(cardTitle) {
+    let tab = cardTitle.toLowerCase();
+    const tabName = tab.split(" ").join("-");
+    navigate(`/dashboard/patient/tabs/${tabName}`);
+  }
 
   return (
-    <Card className="card-bg" sx={{ maxWidth: "100%"}} variant="outlined" onClick={handleClick}>
+    <Card className="card-bg" sx={{ maxWidth: "100%"}} variant="outlined" onClick={() => handleClick(props.cardTitle)}>
       <CardContent>
         <br /> <br />
         <Typography variant="h4" component="div">
