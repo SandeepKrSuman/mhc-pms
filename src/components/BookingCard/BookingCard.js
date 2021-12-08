93f0567877f4e318aec48f281cf5363a0befb909
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-08 16:28:49 +0530

    modified BookingCard

--- src/components/BookingCard/BookingCard.js
+++ src/components/BookingCard/BookingCard.js
@@ -1,25 +1,36 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import AddTaskIcon from '@mui/icons-material/AddTask';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+import { Link } from "react-router-dom";
 
 export default function BookingCard(props) {
-
   return (
-    <Card sx={{ maxWidth: "100%", textAlign: "center"}} variant="outlined">
+    <Card sx={{ maxWidth: "100%", textAlign: "center" }} variant="outlined">
       <CardContent>
         <br />
         <Typography variant="h5" component="div">
-            {props.heading}
+          {props.heading}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {props.subheading}
+          {props.subheading}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ mb: 1.5 }} color="text.secondary">
+          {`₹${props.fee}`}
         </Typography>
         <br />
         <CardActions disableSpacing>
-          <Button variant="contained" color="warning" endIcon={<AddTaskIcon />}>BOOK</Button>
+          <Link className="link-btn" to="/dashboard/tabs/make-payment">
+            <Button
+              variant="contained"
+              color="warning"
+              endIcon={<AddTaskIcon />}
+            >
+              BOOK
+            </Button>
+          </Link>
         </CardActions>
       </CardContent>
     </Card>
