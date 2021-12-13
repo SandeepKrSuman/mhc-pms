cebcc3c9f39ee8a83c06d177bb08caa634248077
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-14 01:16:03 +0530

    card and upi payment options for patient

---
+++ src/components/Dashboards/PatientDash/Tabs/CardPayment.js
@@ -0,0 +1,7 @@
+import CardPay from "../../../Payments/Card/CardPay";
+
+function CardPaymentPatient() {
+  return <CardPay />;
+}
+
+export default CardPaymentPatient;
---
+++ src/components/Dashboards/PatientDash/Tabs/UpiPayment.js
@@ -0,0 +1,7 @@
+import UpiPay from "../../../Payments/UPI/UpiPay";
+
+function UpiPaymentPatient() {
+  return <UpiPay />;
+}
+
+export default UpiPaymentPatient;
