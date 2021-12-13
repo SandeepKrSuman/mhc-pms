197e20b05b7148475204061a24b8a81233df6d1c
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-14 01:12:59 +0530

    modified Patient MakePayment

--- src/components/Dashboards/PatientDash/Tabs/MakePayment.js
+++ src/components/Dashboards/PatientDash/Tabs/MakePayment.js
@@ -1,22 +1,9 @@
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Fab from '@mui/material/Fab';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Dashboard from "../../Dashboard";
 
-function MakePayment(){
-    return (
-        <Container maxWidth="lg" sx={{height: "100vh", textAlign: "center", paddingTop: "20vh", paddingBottom: "3%", }}>
-            <img alt="qr-code" src="/images/paymentQR.png" />
-            <br /> <br />
-            <Typography variant="body1" gutterBottom component="div">
-                Scan the above QR to pay appointment fee of ₹ 1000
-            </Typography>
-            <br /> <br />
-            <Fab href="/downloadFiles/invoice1000.pdf" download="mhc-pms-payment-invoice" color="primary" variant="extended">
-                <FileDownloadIcon /> {" Invoice"}
-            </Fab>
-        </Container>
-    );
+const cardTitles = ["Card Payment", "UPI Payment"];
+
+function MakePaymentPatinet() {
+  return <Dashboard userName="PatientXyZ" cards={cardTitles} lgspace={6} />;
 }
 
-export default MakePayment;
+export default MakePaymentPatinet;
