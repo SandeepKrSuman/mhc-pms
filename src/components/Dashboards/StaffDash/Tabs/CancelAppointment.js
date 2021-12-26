651cf982e0602e387cdf09fb1790b48ab90d2e3a
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-27 00:10:25 +0530

    added search field to cancel for staff

--- src/components/Dashboards/StaffDash/Tabs/CancelAppointment.js
+++ src/components/Dashboards/StaffDash/Tabs/CancelAppointment.js
@@ -1,9 +1,12 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Container from "@mui/material/Container";
 import DashBar from "../../../DashBar/DashBar";
 import AppointmentCard from "../../../Cards/AppointmentCard";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 //search appointment using unique id and store in below array
 const appointments = [
@@ -13,42 +16,85 @@ const appointments = [
 ];
 
 function CancelAppointment() {
-  if (appointments.length > 0) {
+  const [searched, setSearched] = useState(false);
+  const [email, setEmail] = useState("");
+
+  function handleChange(event) {
+    const e = event.target.value;
+    setEmail(e);
+  }
+
+  function handleSearch() {
+    // search the email in db and return the appointmet list
+    setSearched(true);
+  }
+
+  if (!searched) {
     return (
       <Fragment>
-        <DashBar user={"PatientXyZ"} />
-        <Container className="dash-container" maxWidth="md">
-          <Grid container spacing={3}>
-            {appointments.map((appointment, index) => {
-              return (
-                <Grid key={index} item xs={12}>
-                  <AppointmentCard
-                    doc={appointment.doc}
-                    date={appointment.date}
-                  />
-                </Grid>
-              );
-            })}
-          </Grid>
-        </Container>
-      </Fragment>
-    );
-  } else {
-    return (
-      <Fragment>
-        <DashBar user={"UserXyZ"} />
+        <DashBar />
         <Container sx={{ textAlign: "center" }}>
-          <Typography
+          <Stack
+            spacing={0}
+            direction="row"
             sx={{ marginTop: "30vh" }}
-            variant="h5"
-            gutterBottom
-            component="div"
+            justifyContent="center"
           >
-            **Patient does NOT seem to have any appointment.**
-          </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Enter Email ID"
+              type="email"
+              value={email}
+              onChange={handleChange}
+              variant="outlined"
+              autoComplete="off"
+              sx={{ width: "90%" }}
+            />
+            <Button onClick={handleSearch} variant="outlined">
+              Search
+            </Button>
+          </Stack>
         </Container>
       </Fragment>
     );
+  } else if (searched) {
+    if (appointments.length > 0) {
+      return (
+        <Fragment>
+          <DashBar />
+          <Container className="dash-container" maxWidth="md">
+            <Grid container spacing={3}>
+              {appointments.map((appointment, index) => {
+                return (
+                  <Grid key={index} item xs={12}>
+                    <AppointmentCard
+                      doc={appointment.doc}
+                      date={appointment.date}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Container>
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          <DashBar />
+          <Container sx={{ textAlign: "center" }}>
+            <Typography
+              sx={{ marginTop: "30vh" }}
+              variant="h5"
+              gutterBottom
+              component="div"
+            >
+              **Patient does NOT seem to have any appointment.**
+            </Typography>
+          </Container>
+        </Fragment>
+      );
+    }
   }
 }
 
