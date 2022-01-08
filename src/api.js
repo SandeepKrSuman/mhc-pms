7d3c7a486cab984c50ce1de03bbb500d12e9d1be
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-01-08 22:58:52 +0530

    modified baseUrl

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://mhc-pms-server.herokuapp.com/api";
 
 axios.interceptors.request.use(
   (config) => {
