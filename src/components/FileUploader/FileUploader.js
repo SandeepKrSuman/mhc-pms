0236eb6a9053644133b07ce7ab2dd91ab1fa63d9
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-02-02 15:56:46 +0530

    File Upload

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -22,20 +22,24 @@ const useStyle = {
 export default function FileUploader(props) {
   const [openBackdrop, setOpenBackdrop] = useState(false);
   const [fileName, setFileName] = useState(null);
+  const [file, setFile] = useState(null);
 
   function handleChange(e) {
     const fname = e.target.files[0].name;
     setFileName(fname);
+    setFile(e.target.files[0]);
   }
 
-  async function handleSubmit() {
+  async function handleSubmit(e) {
+    e.preventDefault();
     setOpenBackdrop(true);
+    const formData = new FormData();
+    formData.append("pemail", props.pemail);
+    formData.append("demail", props.demail);
+    formData.append("doa", props.doa);
+    formData.append("prescriptionFile", file);
     try {
-      const res = await api.uploadPrescription({
-        pemail: props.pemail,
-        demail: props.demail,
-        doa: props.doa,
-      });
+      const res = await api.uploadPrescription(formData);
       if (res.data.error) {
         setOpenBackdrop(false);
         alert(res.data.errorMsg);
@@ -53,42 +57,47 @@ export default function FileUploader(props) {
 
   return (
     <Fragment>
-      <FormGroup>
-        <label htmlFor={`file-uploader-${props.useKey}`}>
-          <Input
-            onChange={handleChange}
-            accept="image/*, application/pdf"
-            id={`file-uploader-${props.useKey}`}
-            type="file"
-          />
-          <Button
-            style={useStyle}
-            variant="outlined"
-            component="span"
-            startIcon={<PublishIcon />}
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <FormGroup>
+          <label htmlFor={`file-uploader-${props.useKey}`}>
+            <Input
+              onChange={handleChange}
+              accept="image/*, application/pdf"
+              name="prescriptionFile"
+              id={`file-uploader-${props.useKey}`}
+              type="file"
+            />
+            <Button
+              style={useStyle}
+              variant="outlined"
+              component="span"
+              startIcon={<PublishIcon />}
+            >
+              Upload
+            </Button>
+          </label>
+          <br />
+          <Typography
+            sx={{ textAlign: "center" }}
+            variant="caption"
+            color="green"
+            display="block"
+            gutterBottom
           >
-            Upload
-          </Button>
-        </label>
-        <br />
-        <Typography
-          sx={{ textAlign: "center" }}
-          variant="caption"
-          color="green"
-          display="block"
-          gutterBottom
-        >
-          {fileName}
-        </Typography>
-        <br />
+            {fileName}
+          </Typography>
+          <br />
+        </FormGroup>
         <Button
           disabled={fileName ? false : true}
           onClick={handleSubmit}
           variant="contained"
+          sx={{ width: "100%" }}
+          type="submit"
         >
           Submit
         </Button>
-      </FormGroup>
+      </form>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={openBackdrop}
