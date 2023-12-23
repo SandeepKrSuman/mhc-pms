4492ff3d4e7ec0b7f81ea29bc875e4cef43b350f
ahmadfahrezi <ahmadfahrezi8127@gmail.com>
2023-12-23 17:40:14 +0800

    first commit

--- src/api.js
+++ src/api.js
@@ -1,130 +1,134 @@
 import axios from "axios";
 
+// const baseUrl = `${process.env.REACT_APP_BASE}/api`;
 const baseUrl = `${process.env.REACT_APP_BASE}/api`;
 
 axios.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      config.headers["x-auth-token"] = accessToken;
+    (config) => {
+        const accessToken = localStorage.getItem("accessToken");
+        if (accessToken) {
+            config.headers["x-auth-token"] = accessToken;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    Promise.reject(error);
-  }
 );
 
 axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    const originalRequest = error.config;
-    let refreshToken = localStorage.getItem("refreshToken");
-    if (
-      refreshToken &&
-      error.response.status === 401 &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
-      return axios
-        .post(`${baseUrl}/auth/refresh`, { refreshToken: refreshToken })
-        .then((res) => {
-          if (res.status === 200) {
-            localStorage.setItem("accessToken", res.data.accessToken);
-            return axios(originalRequest);
-          }
-        });
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const originalRequest = error.config;
+        let refreshToken = localStorage.getItem("refreshToken");
+        if (
+            refreshToken &&
+            error.response.status === 401 &&
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true;
+            return axios
+                .post(`${baseUrl}/auth/refresh`, { refreshToken: refreshToken })
+                .then((res) => {
+                    if (res.status === 200) {
+                        localStorage.setItem(
+                            "accessToken",
+                            res.data.accessToken
+                        );
+                        return axios(originalRequest);
+                    }
+                });
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 const api = {
-  signup: (body) => {
-    return axios.post(`${baseUrl}/auth/signup`, body);
-  },
+    signup: (body) => {
+        return axios.post(`${baseUrl}/auth/signup`, body);
+    },
 
-  signin: (body) => {
-    return axios.post(`${baseUrl}/auth/signin`, body);
-  },
+    signin: (body) => {
+        return axios.post(`${baseUrl}/auth/signin`, body);
+    },
 
-  refreshToken: (body) => {
-    return axios.post(`${baseUrl}/auth/refresh`, body);
-  },
+    refreshToken: (body) => {
+        return axios.post(`${baseUrl}/auth/refresh`, body);
+    },
 
-  logout: (body) => {
-    return axios.delete(`${baseUrl}/auth/logout`, body);
-  },
+    logout: (body) => {
+        return axios.delete(`${baseUrl}/auth/logout`, body);
+    },
 
-  docList: () => {
-    return axios.get(`${baseUrl}/doctor/list`);
-  },
+    docList: () => {
+        return axios.get(`${baseUrl}/doctor/list`);
+    },
 
-  addNew: (body) => {
-    return axios.post(`${baseUrl}/doctor/new`, body);
-  },
+    addNew: (body) => {
+        return axios.post(`${baseUrl}/doctor/new`, body);
+    },
 
-  updateFee: (body) => {
-    return axios.post(`${baseUrl}/doctor/fee`, body);
-  },
+    updateFee: (body) => {
+        return axios.post(`${baseUrl}/doctor/fee`, body);
+    },
 
-  unverified: () => {
-    return axios.get(`${baseUrl}/users/unverified`);
-  },
+    unverified: () => {
+        return axios.get(`${baseUrl}/users/unverified`);
+    },
 
-  verify: (body) => {
-    return axios.post(`${baseUrl}/users/unverified/verify`, body);
-  },
+    verify: (body) => {
+        return axios.post(`${baseUrl}/users/unverified/verify`, body);
+    },
 
-  reject: (body) => {
-    return axios.delete(`${baseUrl}/users/unverified/reject`, body);
-  },
+    reject: (body) => {
+        return axios.delete(`${baseUrl}/users/unverified/reject`, body);
+    },
 
-  generateStats: () => {
-    return axios.get(`${baseUrl}/generate/stats`);
-  },
+    generateStats: () => {
+        return axios.get(`${baseUrl}/generate/stats`);
+    },
 
-  patientFeedbacks: () => {
-    return axios.get(`${baseUrl}/patient/feedbacks`);
-  },
+    patientFeedbacks: () => {
+        return axios.get(`${baseUrl}/patient/feedbacks`);
+    },
 
-  bookAppointment: (body) => {
-    return axios.post(`${baseUrl}/booking/appointment`, body);
-  },
+    bookAppointment: (body) => {
+        return axios.post(`${baseUrl}/booking/appointment`, body);
+    },
 
-  duePayment: () => {
-    return axios.get(`${baseUrl}/booking/duepayment`);
-  },
+    duePayment: () => {
+        return axios.get(`${baseUrl}/booking/duepayment`);
+    },
 
-  makePayment: (body) => {
-    return axios.post(`${baseUrl}/booking/payment`, body);
-  },
+    makePayment: (body) => {
+        return axios.post(`${baseUrl}/booking/payment`, body);
+    },
 
-  myAppointments: () => {
-    return axios.get(`${baseUrl}/patient/appointments`);
-  },
+    myAppointments: () => {
+        return axios.get(`${baseUrl}/patient/appointments`);
+    },
 
-  cancelAppointment: (body) => {
-    return axios.delete(`${baseUrl}/patient/appointments/cancel`, body);
-  },
+    cancelAppointment: (body) => {
+        return axios.delete(`${baseUrl}/patient/appointments/cancel`, body);
+    },
 
-  prescriptions: () => {
-    return axios.get(`${baseUrl}/patient/prescriptions`);
-  },
+    prescriptions: () => {
+        return axios.get(`${baseUrl}/patient/prescriptions`);
+    },
 
-  writeFeedback: (body) => {
-    return axios.post(`${baseUrl}/patient/feedback/write`, body);
-  },
+    writeFeedback: (body) => {
+        return axios.post(`${baseUrl}/patient/feedback/write`, body);
+    },
 
-  deleteFeedback: (body) => {
-    return axios.post(`${baseUrl}/patient/feedback/delete`, body);
-  },
+    deleteFeedback: (body) => {
+        return axios.post(`${baseUrl}/patient/feedback/delete`, body);
+    },
 
-  uploadPrescription: (body) => {
-    return axios.post(`${baseUrl}/doctor/prescription/upload`, body);
-  },
+    uploadPrescription: (body) => {
+        return axios.post(`${baseUrl}/doctor/prescription/upload`, body);
+    },
 };
 
 export default api;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,182 +20,190 @@ import "./SignIn.css";
 const theme = createTheme();
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function SignIn() {
-  const auth = React.useContext(AuthContext);
+    const auth = React.useContext(AuthContext);
 
-  const [searchParams] = useSearchParams();
-  const sparam = searchParams.get("new");
-  const [succOpen, setSuccOpen] = React.useState(sparam ? true : false);
-  const [errOpen, setErrOpen] = React.useState(false);
-  const [openBackdrop, setOpenBackdrop] = React.useState(false);
-  const [errorMsg, setErrorMsg] = React.useState(
-    "An Error Occured. Try again later."
-  );
+    const [searchParams] = useSearchParams();
+    const sparam = searchParams.get("new");
+    const [succOpen, setSuccOpen] = React.useState(sparam ? true : false);
+    const [errOpen, setErrOpen] = React.useState(false);
+    const [openBackdrop, setOpenBackdrop] = React.useState(false);
+    const [errorMsg, setErrorMsg] = React.useState(
+        "An Error Occured. Try again later."
+    );
 
-  const handleErrorClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
+    const handleErrorClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
 
-    setErrOpen(false);
-  };
-
-  const handleSuccessClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setSuccOpen(false);
-  };
-
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    setOpenBackdrop(true);
-    event.preventDefault();
-    const postData = {
-      email: email,
-      password: password,
+        setErrOpen(false);
     };
 
-    try {
-      const res = await api.signin(postData);
-      if (res.data.error) {
-        setOpenBackdrop(false);
-        setErrorMsg(res.data.errorMsg);
-        setErrOpen(true);
-      } else {
-        const loggedUser = res.data.userType;
-        const { accessToken, refreshToken } = res.data;
-        localStorage.setItem("accessToken", accessToken);
-        localStorage.setItem("refreshToken", refreshToken);
-        setOpenBackdrop(false);
-        auth.setUserType(loggedUser);
-      }
-    } catch (error) {
-      setOpenBackdrop(false);
-      setErrorMsg(error.response.data.errorMsg);
-      setErrOpen(true);
-      console.error(error);
-    }
-  };
+    const handleSuccessClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setSuccOpen(false);
+    };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar
-            alt="Remy Sharp"
-            src="/images/brain.png"
-            sx={{ width: 56, height: 56 }}
-          />
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} validate sx={{ mt: 1 }}>
-            <br /> <br />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              type="email"
-              label="Email Address"
-              name="email"
-              value={email}
-              autoComplete="email"
-              autoFocus
-              onChange={handleEmail}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePassword}
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+    };
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        setOpenBackdrop(true);
+        event.preventDefault();
+        const postData = {
+            email: email,
+            password: password,
+        };
+
+        try {
+            const res = await api.signin(postData);
+            if (res.data.error) {
+                setOpenBackdrop(false);
+                setErrorMsg(res.data.errorMsg);
+                setErrOpen(true);
+            } else {
+                const loggedUser = res.data.userType;
+                const { accessToken, refreshToken } = res.data;
+                localStorage.setItem("accessToken", accessToken);
+                localStorage.setItem("refreshToken", refreshToken);
+                setOpenBackdrop(false);
+                auth.setUserType(loggedUser);
+            }
+        } catch (error) {
+            setOpenBackdrop(false);
+            setErrorMsg(error.response.data.errorMsg);
+            setErrOpen(true);
+            console.error(error);
+        }
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar
+                        alt="Remy Sharp"
+                        src="/images/brain.png"
+                        sx={{ width: 56, height: 56 }}
+                    />
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        validate
+                        sx={{ mt: 1 }}
+                    >
+                        <br /> <br />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            type="email"
+                            label="Email Address"
+                            name="email"
+                            value={email}
+                            autoComplete="email"
+                            autoFocus
+                            onChange={handleEmail}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePassword}
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs></Grid>
+                            <Grid item>
+                                <Link className="link-btn-login" to="/signup">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+            <Snackbar
+                key={"success"}
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                open={succOpen}
+                autoHideDuration={3000}
+                onClose={handleSuccessClose}
             >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs></Grid>
-              <Grid item>
-                <Link className="link-btn-login" to="/signup">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-      <Snackbar
-        key={"success"}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={succOpen}
-        autoHideDuration={3000}
-        onClose={handleSuccessClose}
-      >
-        <Alert
-          onClose={handleSuccessClose}
-          severity="success"
-          sx={{ width: "100%" }}
-        >
-          Registration Successful. Go ahead and login...
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        key={"error"}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={errOpen}
-        autoHideDuration={6000}
-        onClose={handleErrorClose}
-      >
-        <Alert
-          onClose={handleErrorClose}
-          severity="error"
-          sx={{ width: "100%" }}
-        >
-          {errorMsg}
-        </Alert>
-      </Snackbar>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={openBackdrop}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </ThemeProvider>
-  );
+                <Alert
+                    onClose={handleSuccessClose}
+                    severity="success"
+                    sx={{ width: "100%" }}
+                >
+                    Registration Successful. Go ahead and login...
+                </Alert>
+            </Snackbar>
+            <Snackbar
+                key={"error"}
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                open={errOpen}
+                autoHideDuration={6000}
+                onClose={handleErrorClose}
+            >
+                <Alert
+                    onClose={handleErrorClose}
+                    severity="error"
+                    sx={{ width: "100%" }}
+                >
+                    {errorMsg}
+                </Alert>
+            </Snackbar>
+            <Backdrop
+                sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                }}
+                open={openBackdrop}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </ThemeProvider>
+    );
 }
